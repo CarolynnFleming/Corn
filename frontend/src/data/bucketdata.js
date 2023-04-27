16a5c42b298dfb665cfbd247959fe553b6d1cf3c
CarolynnFleming <cfcatpink@gmail.com>
2023-04-26 19:46:23 -0500

    bucketdata

--- frontend/src/data/bucketdata.js
+++ frontend/src/data/bucketdata.js
@@ -1,21 +1,21 @@
 const bucketdata = {
     bucket: [
         {
-            id:'39',
+            id:'1',
             name: 'cheese',
             image:'/public/Cheddar-popcorn.jpg',
             price: 18.99,
             countInStock:10,  
         },
         {
-            id:'40',
+            id:'2',
             name: 'poppin mix',
             image:'/public/Mix.jpg',
             price: 19.99,
             countInStock:10,  
         },
         {
-            id:'41',
+            id:'3',
             name: 'caramel',
             image:'/public/caramel.jpeg',
             price: 22.99,
@@ -23,14 +23,14 @@ const bucketdata = {
               
         },
         {
-            id:'42',
+            id:'4',
             name: 'cheesy caramel mix',
             image:'/public/Mix.jpg',
             price: 23.99,
             countInStock:10,
         },
         {
-            id:'43',
+            id:'5',
             name: 'cheesy caramel',
             image:'/public/Mix.jpg',
             price: 25.99,
@@ -38,7 +38,7 @@ const bucketdata = {
               
         },
         {
-            id:'44',
+            id:'6',
             name: 'nutty caramel mix',
             image:'/public/Mix.jpg',
             price: 26.99,
@@ -46,7 +46,7 @@ const bucketdata = {
               
         },
         {
-            id:'45',
+            id:'7',
             name: 'nutty caramel',
             image:'/public/Mix.jpg',
             price: 29.99,
