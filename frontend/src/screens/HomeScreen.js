18268b33a88452f25784d73339f1fd4168dc7398
CarolynnFleming <cfcatpink@gmail.com>
2023-04-30 23:53:53 -0500

    background

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,19 +3,20 @@ import { Link } from 'react-router-dom'
 
 export default function HomeScreen() {
   return(
-    <div className='row center'style={{background: 'url(/Background.jpg)', backgroundSize:'cover'}}>
+    <div className='row center'style={{background: 'url(/iPhone.jpg)', backgroundSize:'cover'}}>
    
         <div className='home-card'>
-        <img className="home-popcorn"src="./public/2.jpg" alt="popcorn"/>
-        <img className="home-popcorn"src="./public/popcorn.gif" alt="popcorn"/>
-        <img className="home-popcorn"src="./public/3.jpg" alt="popcorn"/>
         
+        <img className="home-popcorn"src="./public/popcorn.gif" alt="popcorn"/>
        
+        
+       <div className='goldenrule'>
         <span className='first'>It all starts with the best popcorn seeds<br/> <span className='second'>We serve Popcorn and Italian Ice<br/> <br/>
         <Link className="button-85 roman" to="/offeringscreen">Go Shopping
         </Link>
         </span>
         </span>
+        </div>
        
         </div>
     </div>
