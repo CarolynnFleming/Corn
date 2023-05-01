f3ba09e00ad100639d6a2dc1699b4e104d9d7df5
CarolynnFleming <cfcatpink@gmail.com>
2023-05-01 01:09:21 -0500

    helpers fixed

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,7 +1,6 @@
 const API = 'http://localhost:5050';
 
 export async function fetchFromAPI(endpoint, choices) {
-
     const { method, body } = { method: 'POST', body: null, ...choices };
 
     const res = await fetch(`${API}/${endpoint}`, {
