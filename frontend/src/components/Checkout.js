0b9f02f8c953b21f5662c8f9fa391b7177409c62
Carolynn Fleming <cfcatpink@gmail.com>
2023-07-24 21:53:28 -0500

    Update Checkout.js

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -18,9 +18,9 @@ const Checkout = () => {
       <span>Items in Cart:  {itemCount}</span><br/>
       
       <span>Cart Total: ${total.toFixed(2)}</span><br/>
-      <span>Service Fee: {stripeServiceFee.toFixed(2)}</span><br/>
+      <span>Service Fee: ${stripeServiceFee.toFixed(2)}</span><br/>
       <br/>
-      <span>Total Payment: ${formatCurrency(total + stripeServiceFee)}</span><br/>
+      <span>Total Payment: {formatCurrency(total + stripeServiceFee)}</span><br/>
      
       <hr />
       <StripeCheckout />
