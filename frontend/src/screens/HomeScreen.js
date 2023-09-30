b24efbd2294b36373e742e52d07a3ac718eb3fae
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 12:08:29 -0500

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,19 +4,33 @@ import SecondBanner from "../components/banners/SecondBanner";
 import ThirdBanner from "../components/banners/ThirdBanner";
 import SixthBanner from "../components/banners/SixthBanner";
 import FifthBanner from "../components/banners/FifthBanner";
-
+import Des1 from "../components/banners/Des1";
+import Des2 from "../components/banners/Des2";
+import Des3 from "../components/banners/Des3";
+import Des4 from "../components/banners/Des4";
+import Des5 from "../components/banners/Des5";
+import Des6 from "../components/banners/Des6";
+import Des7 from "../components/banners/Des7";
 export default function HomeScreen() {
 
 
   return(
    
         <div>
+       <Des1/>
+          
         <MainBanner/>
-       <SecondBanner/>
+        <Des2/>
+       <SeconeBanner/>
+       <Des3/>
        <FourthBanner/>
+       <Des4/>
       <ThirdBanner/>
+      <Des5/>
        <FifthBanner/>
+       <Des6/>
        <SixthBanner/>
+       <Des7/>
        
       
        
