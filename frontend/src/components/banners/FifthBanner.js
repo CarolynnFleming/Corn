2ec8bfbb1a436313c123a61c7b309fea3b6a252c
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 03:19:22 -0500

    Create FifthBanner.js

---
+++ frontend/src/components/banners/FifthBanner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function FifthBanner() {
+  return (
+    <div className='fifthban'>
+        <div className='wordone'>
+            <span> Our rich and savory Cheese Popcorn <br/>is a customer favorite! Our luscious <br/>Caramel Popcorn is distinctly made <br/>from scratch with creamy butter and fresh brown sugar.
+        </span>
+        <div> </div> 
+        </div>
+    <div className='wordtwo'>
+        <span>Our very own expert Poppers<br/> have also created many more tantalizing Flavors of popcorn. Offering our popular Banana Pudding flavor, Cheesy Buffalo, and Dill-flavored popcorn to name a few.
+        </span>
+    </div> 
+    </div>
+  )
+}
