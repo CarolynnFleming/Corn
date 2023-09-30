e408577b389ad8a8a58d33c2085a900d3881b945
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 00:28:24 -0500

    Create SliderData.js

---
+++ frontend/src/components/SliderData.js
@@ -0,0 +1,9 @@
+export const SliderData = [
+    {image:'/public/CheesyCaramel.jpeg'},
+    {image: '/public/Cheese.jpeg'},
+    {image: '/public/SpicyBuffalo.jpeg'},
+    {image: '/public/Dill.jpeg'},
+    {image: '/public/PoppinMix.jpeg'},
+    {image: '/public/CaramelApple.jpeg'},
+    {image: '/public/Caramel.jpeg'}
+];
