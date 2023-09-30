74d33311f6238c43853e7fe22a23f3f8f826af03
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 12:06:39 -0500

    Create Des6.js

---
+++ frontend/src/components/banners/Des6.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import something from '../../assets/something.png'
+export default function Des6() {
+  return (
+    <div className='dec2'><img className='b'src={something} alt='something'/></div>
+  )
+}
