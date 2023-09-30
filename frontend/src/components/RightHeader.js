e02338b0845244b48c3faa9a13e140ceb592642e
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 09:36:21 -0500

    Update RightHeader.js

--- frontend/src/components/RightHeader.js
+++ frontend/src/components/RightHeader.js
@@ -55,8 +55,8 @@ const Ul = styled.ul`
     transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
     top: 0;
     right: 0;
-    height: 100vh;
-    width: 34rem;
+    height: 45vh;
+    width: 20rem;
     padding-top: 3.5rem;
     transition: transform 0.3s ease-in-out;
     li {
