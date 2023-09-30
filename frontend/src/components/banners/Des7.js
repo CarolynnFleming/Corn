c1384ea87aa17c0a796109317e2eb292d76e6fba
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 12:07:15 -0500

    Create Des7.js

---
+++ frontend/src/components/banners/Des7.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import something from '../../assets/something.png'
+export default function Des7() {
+  return (
+    <div className='dec1'><img className='a'src={something} alt='something'/></div>
+  )
+}
