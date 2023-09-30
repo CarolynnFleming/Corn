53d1fe2a193ee2c150e5fe6c1bdb43b7c4bed5e8
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 00:25:47 -0500

    Create FourthBanner.js

---
+++ frontend/src/components/banners/FourthBanner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ice from '../../assets/iice.jpeg'
+import corn from '../../assets/corn-images.jpeg'
+export default function FourthBanner() {
+  return (
+    <div className='fourthban'><img className='pic'src={ice} alt='italianice'/>
+    <img className='cornn' src={corn} alt='corn'/></div>
+  )
+}
