7ca07d6417227ce9c6caeec22d6ece23181691ef
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 00:25:09 -0500

    Create ThirdBanner.js

---
+++ frontend/src/components/banners/ThirdBanner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImageSlider from '../ImageSlider'
+import { SliderData } from '../SliderData'
+export default function ThirdBanner() {
+  return (
+    
+    <div className='thirdban'>
+        <ImageSlider slides={SliderData}/> 
+       
+        </div>
+     
+
+  )
+}
