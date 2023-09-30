0a036dfb1f5d662e12234f168c7723ec1ac0f851
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 00:45:08 -0500

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,7 @@
+import FourthBanner from "../components/banners/FourthBanner";
 import MainBanner from "../components/banners/MainBanner";
+import SeconeBanner from "../components/banners/SeconeBanner";
+import ThirdBanner from "../components/banners/ThirdBanner";
 
 
 export default function HomeScreen() {
