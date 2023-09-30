a2e305b29920a1c625ed4d184b83305aaa7543a0
Carolynn Fleming <cfcatpink@gmail.com>
2023-09-30 00:23:46 -0500

    Create SecondBanner.js

---
+++ frontend/src/components/banners/SecondBanner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SeconeBanner() {
+  return (
+    <div className='secondban'>  
+    <span className='row center bob'>We’re a business dedicated to having the best Gourmet Popcorn 
+    and Italian Ice flavors in the U.S.A!!!!</span>
+    </div>
+  )
+}
