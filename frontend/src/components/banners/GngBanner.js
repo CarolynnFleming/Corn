fb13abdca49c68788bf8e5bc95d4fd3a0abda1e3
Carolynn Fleming <cfcatpink@gmail.com>
2023-10-08 01:14:25 -0500

    Create GngBanner.js

---
+++ frontend/src/components/banners/GngBanner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import grab from '../../assets/Grab.mp4'
+export default function GngBanner() {
+  return (
+    <div className='gng' >
+       <Link to='/offeringscreen' onClick={() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }}>
+       <video autoPlay loop muted className='video'>
+        <source src={grab} type='video/mp4'/>
+       </video>
+       </Link>
+    </div>
+  )
+}
