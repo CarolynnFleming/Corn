22e455b237c1611d99f82a635d9a4486165a849c
Carolynn Fleming <cfcatpink@gmail.com>
2023-10-08 01:16:54 -0500

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,41 +1,54 @@
-import FourthBanner from "../components/banners/FourthBanner";
-import MainBanner from "../components/banners/MainBanner";
-import SecondBanner from "../components/banners/SecondBanner";
-import ThirdBanner from "../components/banners/ThirdBanner";
-import SixthBanner from "../components/banners/SixthBanner";
 import FifthBanner from "../components/banners/FifthBanner";
+// import FourthBanner from "../components/banners/FourthBanner";
+import MainBanner from "../components/banners/MainBanner";
+import SeconeBanner from "../components/banners/SeconeBanner";
+import SixthBanner from "../components/banners/SixthBanner";
+import ThirdBanner from "../components/banners/ThirdBanner";
 import Des1 from "../components/banners/Des1";
+import Ew from "../components/banners/Ew";
 import Des2 from "../components/banners/Des2";
-import Des3 from "../components/banners/Des3";
-import Des4 from "../components/banners/Des4";
-import Des5 from "../components/banners/Des5";
-import Des6 from "../components/banners/Des6";
+// import Des3 from "../components/banners/Des3";
+// import Des4 from "../components/banners/Des4";
+// import Des5 from "../components/banners/Des5";
+
 import Des7 from "../components/banners/Des7";
+// import Inbetween1 from "../components/banners/Inbetween1";
+
+import GngBanner from "../components/banners/GngBanner";
+import Words from "../components/banners/Words";
+import FourthBanner from "../components/banners/FourthBanner";
+// import background from '../../assets/background.jpeg';
 export default function HomeScreen() {
 
 
   return(
    
-        <div>
-       <Des1/>
+        <div  >
+          <Des1/>
           
         <MainBanner/>
         <Des2/>
-       <SecondBanner/>
-       <Des3/>
+        <SeconeBanner/>
+        {/* <Inbetween1/> */}
+        <Ew/>
+        <Words/>
+        
+       <GngBanner/>
+       <Des2/>
+       {/* <FourthBanner/> */}
        <FourthBanner/>
-       <Des4/>
+       <Des1/>
+          
+       
       <ThirdBanner/>
-      <Des5/>
+      <Des2/>
        <FifthBanner/>
-       <Des6/>
+       
        <SixthBanner/>
        <Des7/>
-       
-      
-       
         </div>
 
   
   )
 }
+
