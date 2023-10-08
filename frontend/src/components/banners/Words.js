71c0eeb80ec17ca71848512dd14b948cdc022d16
Carolynn Fleming <cfcatpink@gmail.com>
2023-10-08 01:15:58 -0500

    Create Words.js

---
+++ frontend/src/components/banners/Words.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import word from '../../assets/removed.png'
+export default function Words() {
+  return (
+    <div className='remove'><img className='w' src={word} alt='word'/></div>
+  )
+}
