479e01e43365be635be1e865157a88d8f5770049
Carolynn Fleming <cfcatpink@gmail.com>
2023-10-08 01:37:09 -0500

    Update SixthBanner.js

--- frontend/src/components/banners/SixthBanner.js
+++ frontend/src/components/banners/SixthBanner.js
@@ -8,12 +8,12 @@ export default function SixthBanner() {
      <div className='done'><Link to={'/offeringscreen'} onClick={() => {
       window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
     }}>
-     <video autoPlay loop muted className='shop'>
+     <video autoPlay loop muted playsInline className='shop'>
      <source src={shop} type='video/mp4'/>
     </video> </Link> 
     <Link to={'/contactscreen'} onClick={() => {
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-  }}> <video autoPlay loop muted className='shop'>
+  }}> <video autoPlay loop muted playsInline className='shop'>
      <source src={fund} type='video/mp4'/>
     </video></Link>
     </div>
