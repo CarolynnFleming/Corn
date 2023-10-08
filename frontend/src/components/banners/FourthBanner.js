ac1de3140df82a5c12d991f808f2e269a175cb1f
Carolynn Fleming <cfcatpink@gmail.com>
2023-10-08 01:11:27 -0500

    Update FourthBanner.js

--- frontend/src/components/banners/FourthBanner.js
+++ frontend/src/components/banners/FourthBanner.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import ice from '../../assets/iice.jpeg'
-import corn from '../../assets/corn-images.jpeg'
+import ice from '../../assets/ii.jpg'
+import corn from '../../assets/pp.jpg'
 export default function FourthBanner() {
   return (
-    <div className='fourthban'><img className='pic'src={ice} alt='italianice'/>
-    <img className='cornn' src={corn} alt='corn'/></div>
+    <div className='fourthban' ><img className='pic'src={ice} alt='italianice'/>
+    <img className='cornn' src={corn} alt='corn'/>
+    </div>
   )
 }
