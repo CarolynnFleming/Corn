be6a9af306e7f9e744b91eeba820c4c12f0f1d32
Carolynn Fleming <cfcatpink@gmail.com>
2023-10-08 01:14:58 -0500

    Create Inbetween1.js

---
+++ frontend/src/components/banners/Inbetween1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import corn from '../../assets/corn-images.jpeg'
+export default function Inbetween1() {
+  return (
+    <div ><img className='cornn' src={corn} alt='corn'/></div>
+  )
+}
