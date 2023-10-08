d8c31034e9500ba46296bf0d780c9513b320b960
Carolynn Fleming <cfcatpink@gmail.com>
2023-10-08 01:12:53 -0500

    Update SixthBanner.js

--- frontend/src/components/banners/SixthBanner.js
+++ frontend/src/components/banners/SixthBanner.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import shop from '../../assets/shop.mp4'
+import fund from '../../assets/ff.mp4'
 // import ContactScreen from '../../screens/ContactScreen'
 export default function SixthBanner() {
   return (
-     <div className='done row center'><Link to={'/ContactScreen'}><button className='button-85'><span className='ugh'>Fundraising Available <br/>Please Click to Contact</span></button> </Link> <br/><br/>
-    <Link to={'/offeringscreen'}><button className='button-85'><span className='ugh'> Look Around Our Website <br/>Shop Now!</span></button></Link></div>
+     <div className='done'><Link to={'/offeringscreen'} onClick={() => {
+      window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    }}>
+     <video autoPlay loop muted className='shop'>
+     <source src={shop} type='video/mp4'/>
+    </video> </Link> 
+    <Link to={'/contactscreen'} onClick={() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }}> <video autoPlay loop muted className='shop'>
+     <source src={fund} type='video/mp4'/>
+    </video></Link>
+    </div>
   )
 }
